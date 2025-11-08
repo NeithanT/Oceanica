99cd4718402cf358f984dc801a5fdfed245aee32
avwong <alinarodriguezwong@gmail.com>
2025-11-07 19:27:10 -0600

    Clase de ataques

--- Oceanica/src/main/java/menea/Fighters/Attack.java
+++ Oceanica/src/main/java/menea/Fighters/Attack.java
@@ -1,6 +1,84 @@
 package menea.Fighters;
 
+import java.util.Random;
+import menea.Tiles.Board;
+
 
 public class Attack {
 
+    private Random random;
+    private AttackType tipo;
+
+    //CONSTRUCTOR
+    public Attack(AttackType tipo) {
+        this.tipo = tipo;
+    }
+    
+    public void atacarArea(Board board, int centroX, int CentroY, int radio, int damage, String atacante){
+        
+        int inicioX = centroX - radio;
+        int finX = centroX + radio;
+        int inicioY = CentroY - radio;
+        int finY = CentroY + radio;
+        
+        //ataca una zona cuadrada
+        for (int fila = inicioX; fila <= finX; fila++){
+            for (int columna = inicioY; columna <= finY; columna++){
+                board.getTile(fila, columna).daño(damage, atacante);
+            }
+        }
+    }
+    
+    public void atacarLinea (Board board, int inicioX, int inicioY, String direccion, int alcance, int damage, String atacante){
+        //ataca en linea recta hacia abajo, arriba, der o izq
+        for (int i = 1; i <= alcance; i++){
+        
+            switch (direccion.toLowerCase()) {
+                case "arriba":
+                    inicioX -= i;
+                    break;
+                case "abajo":
+                    inicioX += i;
+                    break;
+                case "izquierda":
+                    inicioY -= i;
+                    break;
+                case "derecha":
+                    inicioY += i;
+                    break;
+            }
+            
+            board.getTile(inicioX, inicioY).daño(damage, atacante);
+        }
+    }
+    
+    public void atacarRandomTile(Board board, int damage, String atacante){
+        int fila = random.nextInt(board.getROWS());
+        int columna = random.nextInt(board.getCOLUMNS());
+        board.getTile(fila, columna).daño(damage, atacante);
+    }
+    
+    public int[] getRandomTile(Board board) {
+        return new int[]{
+            random.nextInt(board.getROWS()),
+            random.nextInt(board.getCOLUMNS())
+        };
+    }
+    
+    //GETTERS Y SETTERS
+
+    public AttackType getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(AttackType tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    
+    
+    
+    
+    
 }
