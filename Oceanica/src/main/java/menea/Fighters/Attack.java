38999f90da097c273db140c7098f7190c8e8127a
Melissa <melissa.rojas.e418.com>
2025-11-07 21:47:50 -0600

    Merge branch 'main' of https://github.com/NeithanT/Oceanica
    # Conflicts:
    #	Oceanica/src/main/java/menea/Client/Console/FrameConsole.form
    #	Oceanica/src/main/java/menea/Client/Console/FrameConsole.java

--- Oceanica/src/main/java/menea/Fighters/Attack.java
+++ Oceanica/src/main/java/menea/Fighters/Attack.java
@@ -1,6 +1,14 @@
 package menea.Fighters;
 
+import java.util.Random;
 
-public class Attack {
 
+
+public abstract class Attack {
+     protected Random random; //protected para que sus clases hijas sin importar el paquete, puedan utilizarlos incluso sin gets sets 
+    protected AttackType type;
+    
+    public Attack() {
+        this.random = new Random();
+    }
 }
