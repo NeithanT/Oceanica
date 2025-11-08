a9b2f930b67859b2832b7786813ce1680846bae7
Melissa <melissa.rojas.e418.com>
2025-11-08 10:27:18 -0600

    Agregué Player en CC, tengo que cambiar todo Command Attack aún, agregué command SelecktA

--- Oceanica/src/main/java/menea/Models/CommandAttack.java
+++ Oceanica/src/main/java/menea/Models/CommandAttack.java
@@ -6,57 +6,30 @@ import menea.Tiles.HitResult;
 
 
 public class CommandAttack extends Command {
- 
-    private final Random ramdom = new Random();
     
     public CommandAttack() { }
 
     @Override public String name(){ return "ATTACK"; }
-    @Override public String help(){ return "ATTACK -> envía un ataque aleatorio (10-20)"; }
+    @Override public String help(){ //TODO: agregar los ataques de Alina 
+        return "ATTACK <método> [parámetros] -> Ejecuta un ataque específico\n" +  //los parámetros son la info extra que el usuario debe ingresar estos varían dependiendo del ataque y así 
+               "Métodos disponibles dependen de tu tipo de ataque seleccionado:\n\n" +
+               "FISH_TELEPHATY:\n" +
+               "  - ATTACK CARDUMEN\n" +
+               "  - ATTACK SHARK\n" +
+               "  - ATTACK PULP\n\n" +
+               "THUNDERS_UNDER_THE_SEA:\n" +
+               "  - ATTACK THUNDERRAIN\n" +
+               "  - ATTACK POSEIDON\n" +
+               "  - ATTACK EEL\n\n" +
+               "RELEASE_THE_KRAKEN:\n" +
+               "  - ATTACK TENTACLES\n" +
+               "  - ATTACK BREATH <fila> <col> <direccion>\n" +
+               "  - ATTACK KRAKEN"; 
+    }
 
     @Override
-    public CommandResult execute(CommandContext ctx, String[] args){
-            if (args.length < 2) {
-                return CommandResult.fail("Uso: attack <fila> <columna>");
-            }
-            int fila, col;
-        try {
-            fila = Integer.parseInt(args[0]);
-            col  = Integer.parseInt(args[1]);
-        } catch (NumberFormatException e) {
-            return CommandResult.fail("Fila y columna deben ser números enteros.");
-        }
-
+    public CommandResult execute(CommandContext ctx, String[] args) {
         
-        
-        int dmg = 10 + ramdom.nextInt(11);
-        // convertir a 0 based
-        int r = fila - 1;
-        int c = col  - 1;
-
-        Board board = ctx.board();
-        if (!board.inBounds(r, c)) {
-            return CommandResult.fail("Fuera de rango. Fila: 1.." + board.getROWS() + ", Columna: 1.." + board.getCOLUMNS());
-        }
-
-        HitResult res = board.attackAt(r, c);
-        switch (res) {
-            case HIT:
-                ctx.console().log("Impacto en (" + fila + "," + col + ").");
-                break;
-            case DESTROYED:
-                ctx.console().log("Celda (" + fila + "," + col + ") destruida.");
-                break;
-            case MISS:
-                ctx.console().log("Sin efecto en (" + fila + "," + col + ").");
-                break;
-        }
-
-       
-        ctx.console().refreshBoard();
-
-        return CommandResult.ok(
-            "Ataque a (" + fila + "," + col + ") con " + dmg + " de daño. " + board.resumen()
-        );
     }
+    
 }
--- Oceanica/src/main/java/menea/Models/CommandContext.java
+++ Oceanica/src/main/java/menea/Models/CommandContext.java
@@ -1,6 +1,7 @@
 package menea.Models;
 
 import menea.Client.Console.FrameConsole; 
+import menea.Player.Player;
 import menea.Server.Client;
 import menea.Tiles.Board;
 
@@ -9,13 +10,24 @@ public class CommandContext {
     private final FrameConsole console;
     private final Client clientUI;
     private final Board board;
+    private final Player player;
 
-    public CommandContext(FrameConsole console, Client clientUI, Board board) {
+    public CommandContext(FrameConsole console, Client clientUI, Board board, Player player) {
         this.console = console;
         this.clientUI = clientUI;
         this.board = board;
+        this.player = player;
     }
-    public FrameConsole console(){ return console; }
-    public Client client(){ return clientUI; }
-    public Board board(){ return board; }
+    public FrameConsole console(){ 
+        return console; 
+    }
+    public Client client(){ 
+        return clientUI; 
+    }
+    public Board board(){ 
+        return board; 
+    }
+    public Player player(){
+        return player;
+    }
 }
